b21abf5fa0c65171c3214571070ab8632b67d9af
Toàn Từ <toantu03@gmail.com>
2025-07-26 10:45:16 +0700

    Feat: introduce front controller

--- src/main.py
+++ src/main.py
@@ -1,12 +1,4 @@
 import uvicorn
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("controllers.front_controller:app", host="127.0.0.1", port=8000, reload=True)
