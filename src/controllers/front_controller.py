b75cd4e60f8ddd03300124123adcc5e2bed29da8
BCKhang <138310391+BCKhang@users.noreply.github.com>
2025-08-25 19:28:30 +0700

    cors change

--- src/controllers/front_controller.py
+++ src/controllers/front_controller.py
@@ -58,7 +58,8 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:8080"],
+        allow_origins=["*"
+    ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH"],
     allow_headers=["*"],
